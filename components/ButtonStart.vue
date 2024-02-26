5fc82c23c6ff71e70053fe9243b2e6136f347e12
Olga Nichiporuk <sfjkkj@mail.ru>
2024-02-26 19:13:02 +0300

    db

---
+++ components/ButtonStart.vue
@@ -0,0 +1,33 @@
+<script setup>
+function test() {
+    document.querySelectorAll('.hello').forEach(element => {
+        element.style.display = 'none';
+    });
+    document.querySelector('.container').style.display = 'flex';
+    
+}
+
+</script>
+<template>
+        <button class="hello" id="button" @click="test">Старт</button>
+</template>
+  
+
+<style lang="css">
+
+
+
+#button {
+    margin-top: 50px;
+    width: 400px;
+    font-size: 45px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    background-color: blueviolet;
+    border-radius: 20px;
+    align-items: center;
+    color: white;
+    cursor: pointer;
+    letter-spacing: 0.4mm;
+}
+</style>
+
---
+++ core/interface/challenge.interface.ts
@@ -0,0 +1,7 @@
+export interface IChallenge {
+    question: String,
+    answer_1: String,
+    answer_2: String,
+    answer_3: String,
+    answer_4: String,
+}
